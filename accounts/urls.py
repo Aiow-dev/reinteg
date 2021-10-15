14566a3d3671a222de8e06e34dd16a8d46c35604
ehmatthes <eric@example.com>
2021-10-15 23:55:11 +0300

    fixed launch error

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
 from .views import SignUpView
-from .views import profile_show
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name='signup'),
-    path('profile/<int:pk>/', profile_show, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 """View of accounts - registration (use CustomUser, CustomUserCreationForm)."""
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.models import User
-from django.http import HttpResponse
+# from django.contrib.auth.models import User
+# from django.http import HttpResponse
+
 from .forms import CustomUserCreationForm
 
 # Create your views here.
@@ -15,6 +16,6 @@ class SignUpView(CreateView):
     template_name = 'registration/signup.html'
 
 
-def profile_show(request, pk):
-    current_user = User.objects.get(user=request.user.id)
-    return HttpResponse(current_user.username)
+# def profile_show(request, pk):
+#     current_user = User.objects.get(user=request.user.id)
+#     return HttpResponse(current_user.username)
